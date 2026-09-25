a15181b4371522089ddaa46eb8b9a2730d18b9aa
Mathias M <mamo0017@stud.ek.dk>
2026-09-25 14:50:16 +0200

    Implementeret alle funktioner

---
+++ basal_python_repetition.py
@@ -0,0 +1,22 @@
+def return_hello():
+    return "Hello, World!"
+
+
+def average_of_two_numbers(a, b):
+    return (a + b) / 2
+
+
+def rectangle_area(w, h):
+    return w * h
+
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5 / 9
+
+
+def celsius_to_fahrenheit(c):
+    return c * 9 / 5 + 32
+
+
+def echo(text):
+    return text[:3] * 3
